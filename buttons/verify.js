11b9838a9a9c6f494abf8b30519daebc9244d560
dc: @himeji <tobin.post@gmail.com>
2024-01-22 17:13:40 +0100

    🎨 [CODE] improved structure/format

--- buttons/verify.js
+++ buttons/verify.js
@@ -1,11 +1,18 @@
-const { verifyRole } = require('../config.json');
+const { verifyRole } = require("../config.json");
 
 module.exports = {
-	id: 'verify_button',
-	permissions: [],
-	run: async (client, interaction) => {
-		await interaction.member.roles.add(verifyRole);
-        if (interaction.member.roles.cache.get(verifyRole)) return interaction.reply({ content: `${interaction.user}, You were already verified.`, ephemeral: true })
-        return interaction.reply({ content: `✅ ${interaction.user}, You are verified.`, ephemeral: true })
-	}
+  id: "verify_button",
+  permissions: [],
+  run: async (client, interaction) => {
+    await interaction.member.roles.add(verifyRole);
+    if (interaction.member.roles.cache.get(verifyRole))
+      return interaction.reply({
+        content: `${interaction.user}, You were already verified.`,
+        ephemeral: true,
+      });
+    return interaction.reply({
+      content: `✅ ${interaction.user}, You are verified.`,
+      ephemeral: true,
+    });
+  },
 };
--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds, 
-		GatewayIntentBits.GuildMessages, 
-		GatewayIntentBits.GuildPresences, 
-		GatewayIntentBits.GuildMessageReactions, 
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent
-	], 
-	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
 });
 
-const fs = require('fs');
-const config = require('./config.json');
-require('dotenv').config() // remove this line if you are using replit
+const fs = require("fs");
+const config = require("./config.json");
+require("dotenv").config(); // remove this line if you are using replit
 
-client.commands = new Collection()
-client.aliases = new Collection()
+client.commands = new Collection();
+client.aliases = new Collection();
 client.slashCommands = new Collection();
 client.buttons = new Collection();
 client.prefix = config.prefix;
 
 module.exports = client;
 
-
-fs.readdirSync('./handlers').forEach((handler) => {
-  require(`./handlers/${handler}`)(client)
+fs.readdirSync("./handlers").forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
 });
 
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
